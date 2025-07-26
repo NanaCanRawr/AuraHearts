50143377a18fa42a5eaf11ed235571c36e01fa1e
Ekna <eknaretheshbabu@gmail.com>
2025-07-26 22:02:59 +0400

    game.js

---
+++ game.js
@@ -0,0 +1,7 @@
+// game.js
+const canvas = document.getElementById("gameCanvas");
+const ctx = canvas.getContext("2d");
+
+// Just a test for now – draw a purple square
+ctx.fillStyle = "#a368fc";
+ctx.fillRect(100, 100, 50, 50);
